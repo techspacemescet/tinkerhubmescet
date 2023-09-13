260720cf2d04e941f57d15437e467db72bca8e95
techspacemescet <techspacemescet@gmail.com>
2023-09-13 13:45:40 +0530

    a

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ var button = document.getElementById('bars');
 button.addEventListener(('click',()=>{
     button.style.display = 'none';
 }))
-function showMenu(){
-    button.style.display = 'none';
-    navLinks.style.right ="0";
-}
+    function showMenu(){
+        button.style.display = 'none';
+        navLinks.style.right = "0";
+    }
 function hideMenu(){
     button.style.display = 'block';
     navLinks.style.right ="-200px";
 }
+
 
